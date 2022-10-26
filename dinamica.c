df7493173fd0b930bc301ec55bd466b3576c643a
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2022-10-26 18:34:15 -0300

    Update dinamica.c

--- dinamica.c
+++ dinamica.c
@@ -9,34 +9,33 @@ void dinamica(int **matriz, Povo *povo, int qtdpovos, int distancia, int peso){
     Solucao *n;
 
     t = (int *)malloc((peso+1)*(sizeof(int)));
-	s = (Solucao **)malloc((peso+1)*(sizeof(Solucao *)));
+    s = (Solucao **)malloc((peso+1)*(sizeof(Solucao *)));
 	
     t[0]=0;
-	s[0]=NULL;
+    s[0]=NULL;
     
     for(int x = 1; x <= peso; x++){
-		t[x] = 0;
-		s[x] = NULL;
+	t[x] = 0;
+	s[x] = NULL;
 		
-		for(int i = 0;i < qtdpovos; i++){
-			if(povo[i].peso <= x){
-				if((t[x-povo[i].peso] + povo[i].habilidade) > t[x]){
-					t[x] = t[x-povo[i].peso] + povo[i].habilidade;
-					s[x] = s[x-povo[i].peso];
-					n = (Solucao *)malloc(1 * (sizeof(Solucao)));
-					n->item = i;
-					n->next = s[x];
-					s[x] = n;
-				}
-				
-			}
-		}
-	}
+	for(int i = 0;i < qtdpovos; i++){
+	    if(povo[i].peso <= x){
+		if((t[x-povo[i].peso] + povo[i].habilidade) > t[x]){
+		    t[x] = t[x-povo[i].peso] + povo[i].habilidade;
+		    s[x] = s[x-povo[i].peso];
+		    n = (Solucao *)malloc(1 * (sizeof(Solucao)));
+		    n->item = i;
+		    n->next = s[x];
+		    s[x] = n;
+		}		
+	     }
+	  }
+      }
     
-    printf("\nTotal value of the items in the Knapsack is %d.\nThe knapsack containts the following items :\n", t[peso]);
-
+    printf("\nHabilidade total: %d.\nCaminho e quantidade de itens: ", t[peso]);
     while(s[peso] != NULL){
 		printf("Item- %d\n", (s[peso]->item)+1);
 		s[peso] = s[peso]->next;
 	}
+    printf("\n");
 }
